255e5a1a20383a7fa64cca2865e00c565d33068c
lSalless <saleslaissa@gmail.com>
2025-08-01 16:19:08 -0300

    Adiciona etapa_1 como pasta comum

---
+++ curso_python/etapa_1/tipo.py
@@ -0,0 +1,22 @@
+# 1
+# Crie três variáveis com os seguintes valores: um número inteiro, um número decimal e uma string. Use type() para imprimir o tipo de cada uma.
+
+
+n1 = 10
+n2 = 1.5
+st = "Nome"
+print((type(n1)), (type(n2)), (type(st)), sep='\n',)
+
+#2 
+#Escreva um código que armazene True em uma variável e verifique seu tipo com type().
+
+sol = True 
+print(f'{type(sol)}') 
+
+
+#3
+#Crie um dicionário chamado pessoa com as chaves nome, idade e altura. Atribua valores e imprima o conteúdo.
+
+pessoa = {"nome":"Laíssa", "idade":26, "altura": 1.70}
+print(pessoa) 
+
